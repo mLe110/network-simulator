7245cd1dfd10856db9c35fd35afd968a990b560e
Matthias Leitner <matthiasleitner@gmx.at>
2020-04-07 00:02:21 +0200

    updated run script

--- run_scripts/run_network_setup.py
+++ run_scripts/run_network_setup.py
@@ -7,7 +7,7 @@ docker_api = docker.APIClient(base_url='unix://var/run/docker.sock')
 
 
 def run_container(args_config):
-    container = docker_client.containers.run(image="mle110/ns:0.2",
+    container = docker_client.containers.run(image="ns:optimized",
                                              name=args_config.ns_container_name,
                                              ports={'5000/tcp': 5000},
                                              environment={
@@ -47,6 +47,7 @@ def setup_network_service_network(args_config):
     container_id = run_container(args_config)
     mount_container_net_namespace(container_id)
     close_docker_clients()
+    return container_id
 
 
 if __name__ == "__main__":
@@ -57,4 +58,5 @@ if __name__ == "__main__":
                                                                           "use.")
     args = parser.parse_args()
 
-    setup_network_service_network(args)
+    net_ns = setup_network_service_network(args)
+    print(net_ns)
